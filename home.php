0e606d1630b291bed3792e42d2f63e2b9583d6b2
Ade Fian <fianade8@gmail.com>
2019-12-04 23:06:07 +0700

    Menambahkan home page dan folder asset

--- home.php
+++ home.php
@@ -5,17 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Nonegram</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="asset/style.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
 </head>
 <body>
-    <div id="header">
-        Header
-    </div>
-    <div id="content">
-        Content
-    </div>
-    <div id="footer">
-        Footer
-    </div>
+    
+
+    
+    <script src="main.js"></script>
 </body>
 </html>
